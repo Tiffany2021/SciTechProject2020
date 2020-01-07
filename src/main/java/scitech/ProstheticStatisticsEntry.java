0b66e77ed6904b6d8fb3dc5ae6d27c8360f5f11b
Tiffany Li <tiffli2019@gmail.com>
2020-01-06 18:18:13 -0700

    Added Circumference Method and Test code. However, unable to pass test.

--- src/main/java/scitech/ProstheticStatisticsEntry.java
+++ src/main/java/scitech/ProstheticStatisticsEntry.java
@@ -1,6 +1,29 @@
 package scitech;
 
+import java.util.Scanner;
+
 public class ProstheticStatisticsEntry{
     
+    public static double circumference;
     
+    
+    public static void main(String[] args) {
+
+
+    }
+    
+    public static void Measurements(){
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the measured CIRCUMFERENCE: ");
+        circumference = scanner.nextDouble();
+        
+
+    }
+    
+    public static double Circumference() {
+        return circumference;
+    }
+
 }
--- src/test/java/scitech/ProstheticStatisticsEntryTest.java
+++ src/test/java/scitech/ProstheticStatisticsEntryTest.java
@@ -1,23 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package scitech;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
 import org.junit.Test;
-
-
 
 /**
  *
  * @author tiff2
  */
 public class ProstheticStatisticsEntryTest {
+    private static ProstheticStatisticsEntry stat;
+            
+    double circumference = ProstheticStatisticsEntry.Circumference();
+    
+    @BeforeClass
+    public static void setup(){
+        stat = new ProstheticStatisticsEntry();
+    }
     
     @Test
-    public void alwaysFail(){
-        //fail("fail");
+    public void testSayHello() throws Exception {
+        double result = stat.Circumference();
+        
+        assertEquals("Did not receive expected measurement: " + result, circumference, result);
     }
+    
 }
