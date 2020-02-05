eb550df4669186972fca277302d6356cbd78c92b
Tiffany Li <tiffli2019@gmail.com>
2020-02-04 23:26:34 -0700

    Forgot to save Test. Question on returning variables and TDD.

--- src/test/java/scitech/ProjectMainTestClass.java
+++ src/test/java/scitech/ProjectMainTestClass.java
@@ -9,7 +9,24 @@ import org.junit.Test;
  * @author tiff2
  */
 public class ProjectMainTestClass {
+    private PatientStatistics patientStatistics;
+    private TestPatientDataSource patientDatasource;
     
+    @Before
+    public void setup(){
+        patientDatasource = new TestPatientDataSource();
+        patientStatistics = new PatientStatistics(patientDatasource);
+    }
+    
+//    @Test
+//    public void testbuildArrayList() throws Exception {
+//        patientDatasource.setName("Iggy");
+//        patientDatasource.setAge(25);
+
+//        String result = greeter.sayHappyBirthday();
+
+//        assertEquals("Did not receive expected greeting: " + result, "Happy Birthday, Iggy! You are 25 years old!", result);
+//    }
     
     
 }
