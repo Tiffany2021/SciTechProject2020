39fa6be15a777320a982ca8bafdba6f7147736b3
Tiffany Li <tiffli2019@gmail.com>
2020-06-10 15:46:26 -0600

    Solved Libs Problem

--- src/main/java/scitech/WriteToFile.java
+++ src/main/java/scitech/WriteToFile.java
@@ -2,6 +2,7 @@
 package scitech;
 import org.apache.commons.io.FileUtils;
 import java.io.File;
+import java.nio.charset.Charset;
 /**
  *
  * @author tiff2
@@ -11,15 +12,26 @@ public class WriteToFile {
     public static void main(String[] args){
         
         String name = "Unknown";
+        String greeting = "Hey";
         
-        if(args.length > 0){
+        if(args.length == 1){
             name = args[0];
+        }
+        else if(args.length == 2){
+            name = args[0];
+            greeting = args[1];
+        }
+        else if(args.length > 2){
+            throw new RuntimeException("Too many arguments");
         }
         
         try{
+            boolean append = true;
             
             File testFile = FileUtils.getFile("Desktop/ABCD.txt");
-            FileUtils.write(testFile, "Hello again, " + name);
+            FileUtils.write(testFile, greeting + ", " + name + "\n", Charset.forName("UTF-8"), append);
+            
+            
         
         }catch (Exception e){
             e.printStackTrace();
